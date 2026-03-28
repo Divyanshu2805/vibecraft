5879128969c96848501088f7e7bb3b63bb34b6f0
Divyanshu Agrahari <divyanshu11235@gmail.com>
2026-03-28 11:10:10 +0530

    Initialize VibeCraft Spring Boot starter project

---
+++ src/main/java/com/java/vibecraft/VibecraftApplication.java
@@ -0,0 +1,13 @@
+package com.java.vibecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibecraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibecraftApplication.class, args);
+	}
+
+}

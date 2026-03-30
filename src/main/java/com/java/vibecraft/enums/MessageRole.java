0f750fde3528c0e2baaf963d83b8908a73dfc119
Divyanshu Agrahari <divyanshu11235@gmail.com>
2026-03-30 13:10:38 +0530

    feat: add project file, preview, and chat entities

---
+++ src/main/java/com/java/vibecraft/enums/MessageRole.java
@@ -0,0 +1,5 @@
+package com.java.vibecraft.enums;
+
+public enum MessageRole {
+    USER, ASSISTANT, SYSTEM, TOOL
+}
---
+++ src/main/java/com/java/vibecraft/enums/PreviewStatus.java
@@ -0,0 +1,5 @@
+package com.java.vibecraft.enums;
+
+public enum PreviewStatus {
+    CREATING, RUNNING, FAILED, TERMINATED
+}

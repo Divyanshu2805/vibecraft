9cb1aa92aad590ab7fd16312e17927514b6c7a33
Divyanshu Agrahari <divyanshu11235@gmail.com>
2026-04-02 18:06:41 +0530

    feat: add billing controller for plans, subscriptions, and checkout

---
+++ src/main/java/com/java/vibecraft/controller/BillingController.java
@@ -0,0 +1,46 @@
+package com.java.vibecraft.controller;
+
+import com.java.vibecraft.dto.subscription.*;
+import com.java.vibecraft.service.PlanService;
+import com.java.vibecraft.service.SubscriptionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class BillingController {
+
+    private final PlanService planService;
+    private final SubscriptionService subscriptionService;
+
+    @GetMapping("/api/plans")
+    public ResponseEntity<List<PlanResponse>> getAllPlans() {
+        return ResponseEntity.ok(planService.getAllActivePlans());
+    }
+
+    @GetMapping("/api/me/subscription")
+    public ResponseEntity<SubscriptionResponse> getMySubscription() {
+        Long userId = 1L;
+        return ResponseEntity.ok(subscriptionService.getCurrentSubscription(userId));
+    }
+
+    @PostMapping("/api/payments/checkout")
+    public ResponseEntity<CheckoutResponse> createCheckoutResponse(
+            @RequestBody CheckoutRequest request
+    ) {
+        Long userId = 1L;
+        return ResponseEntity.ok(subscriptionService.createCheckoutSessionUrl(request, userId));
+    }
+
+    @PostMapping("/api/payments/portal")
+    public ResponseEntity<PortalResponse> openCustomerPortal() {
+        Long userId = 1L;
+        return ResponseEntity.ok(subscriptionService.openCustomerPortal(userId));
+    }
+
+}

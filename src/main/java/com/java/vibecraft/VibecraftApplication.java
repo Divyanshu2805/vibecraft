36837a4dc609bf349f429c74a8637026434b5eaf
Divyanshu Agrahari <divyanshu11235@gmail.com>
2026-04-03 10:38:39 +0530

    fix: force JVM default timezone to Asia/Kolkata for Postgres connections on Windows

--- src/main/java/com/java/vibecraft/VibecraftApplication.java
+++ src/main/java/com/java/vibecraft/VibecraftApplication.java
@@ -3,10 +3,13 @@ package com.java.vibecraft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class VibecraftApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(VibecraftApplication.class, args);
 	}
 

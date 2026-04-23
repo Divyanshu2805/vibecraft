fd3dc3ba438672c21911d68c7f751937dc778df8
Divyanshu Agrahari <divyanshu11235@gmail.com>
2026-04-23 20:26:21 +0530

    feat: implement real signup/login logic in AuthServiceImpl

--- src/main/java/com/java/vibecraft/service/impl/AuthServiceImpl.java
+++ src/main/java/com/java/vibecraft/service/impl/AuthServiceImpl.java
@@ -3,18 +3,58 @@ package com.java.vibecraft.service.impl;
 import com.java.vibecraft.dto.auth.AuthResponse;
 import com.java.vibecraft.dto.auth.LoginRequest;
 import com.java.vibecraft.dto.auth.SignupRequest;
+import com.java.vibecraft.dto.auth.UserProfileResponse;
+import com.java.vibecraft.entity.User;
+import com.java.vibecraft.error.BadRequestException;
+import com.java.vibecraft.mapper.UserMapper;
+import com.java.vibecraft.repository.UserRepository;
+import com.java.vibecraft.security.AuthUtil;
 import com.java.vibecraft.service.AuthService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class AuthServiceImpl implements AuthService {
+
+    UserRepository userRepository;
+    UserMapper userMapper;
+    PasswordEncoder passwordEncoder;
+    AuthUtil authUtil;
+    AuthenticationManager authenticationManager;
+
     @Override
     public AuthResponse signup(SignupRequest request) {
-        return null;
+
+        userRepository.findByUsername(request.username()).ifPresent(user -> {
+            throw new BadRequestException("User already exists with username: "+request.username());
+        });
+
+        User user = userMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(request.password()));
+        user = userRepository.save(user);
+
+        String token = authUtil.generateAccessToken(user);
+        return new AuthResponse("dummy", userMapper.toUserProfileResponse(user));
     }
 
     @Override
     public AuthResponse login(LoginRequest request) {
-        return null;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.username(), request.password())
+        );
+
+        User user = (User) authentication.getPrincipal();
+
+        String token = authUtil.generateAccessToken(user);
+        return new AuthResponse(token, userMapper.toUserProfileResponse(user));
     }
 }

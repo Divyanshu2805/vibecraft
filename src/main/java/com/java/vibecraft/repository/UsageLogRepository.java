7159905224ba318fbd42821d7078b7b726cc6cb4
Divyanshu Agrahari <divyanshu11235@gmail.com>
2026-05-07 22:06:24 +0530

    feat: add daily usage log repository

---
+++ src/main/java/com/java/vibecraft/repository/UsageLogRepository.java
@@ -0,0 +1,13 @@
+package com.java.vibecraft.repository;
+
+import com.java.vibecraft.entity.UsageLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Repository
+public interface UsageLogRepository extends JpaRepository<UsageLog, Long> {
+    Optional<UsageLog> findByUserIdAndDate(Long userId, LocalDate today);
+}
